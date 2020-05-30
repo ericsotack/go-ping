823c8dd880c4b10bef32e2428b64c9509176ccb5
Eric Sotack <elsotack@BIG-RIG.localdomain>
2020-05-30 19:33:15 -0400

    Implemented command line parsing

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	//"golang.org/x/net/icmp"
+	"log"
+	"net"
+	//"sync"
+	"time"
+)
+
+const billion = 1000000000
+
+type Command struct {
+	addr 		net.IP
+	hostname 	string
+	count 		uint
+	ttl 		uint
+	interval	time.Duration
+	deadline	time.Duration
+	timeout		time.Duration
+}
+
+func sendPing() {
+	// start := time.now()
+}
+
+
+func receivePing() {
+	// create icmp listener
+	//end := time.now()
+}
+
+func parseArgs() (*Command, error){
+	var hostname string = ""	// default val
+	var count uint
+	var ttl uint
+	var interval int64
+	var deadline int64
+	var timeout int64
+
+	flag.UintVar(&count, "c", 4, "The number of pings to be sent out (default = 4).")
+	flag.UintVar(&ttl, "t", 255, "The ttl for the ping (TTL, default = 255).")
+	flag.Int64Var(&interval, "i", 1, "The interval (in seconds) to send pings out at (default = 1).")
+	flag.Int64Var(&deadline, "w", 0, "The number of seconds allowed for all ping requests to finish (default = 0). 0 indicates unlimited time.")
+	flag.Int64Var(&timeout, "W", 1, "The number of seconds waited for a response for each packet (default = 4).")
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		return nil, errors.New(fmt.Sprintf("Error parsing arguments: no hostname or IP address specified."))
+	} else if flag.NArg() > 1 {
+		return nil, errors.New(fmt.Sprintf("Error parsing arguments: cannot determine hostname or IP address from %v", flag.Args()))
+	}
+	endpoint := flag.Arg(0)
+
+	ip := net.ParseIP(endpoint)
+	if ip == nil {
+		ips, err := net.LookupIP(endpoint)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("Unable to resolve hostname: %s", endpoint))
+		}
+		hostname = endpoint
+		ip = ips[0]	// defaults to the first IP that this hostname resolves to
+	}
+
+	return &Command{ ip, hostname, count, ttl, time.Duration(interval * billion), time.Duration(deadline * billion), time.Duration(timeout * billion)}, nil
+}
+
+
+func main() {
+	cmd, err := parseArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", *cmd)
+
+}
